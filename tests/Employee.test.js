22f1d4d21c2526805fd28e8786c17e0551038883
Antonio Marino <marino.antonio375@gmail.com>
2021-11-05 11:51:47 -0400

    test is now working

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,23 +1,22 @@
-class Employee {
-  constructor (name, id, email, role = 'Employee') {
-      this.name = name;
-      this.id = id;
-      this.email = email; 
-      this.role = role;
-  };
-  getName () {
-      return this.name.toUpperCase();
-  };
-  getId () {
-      return this.id;
-  };
-  getEmail () {
-      return this.email;
-  };
-  getRole () {
-      return this.role;
-  };
+const Employee = require("../lib/Employee");
 
-};
+describe('Employee class', () => { 
+    // constants for employee to test.
+    const eName = 'Antonio Marino';
+    const eId = '0004';
+    const eMail = 'marinoantonio@gmail.com';
+    
+    it("New employee returns a role of employee", () =>{
+        expect(new Employee(eName, eId, eMail).getRole()).toBe('Employee')
+    });
+    it("New Employee returns a name in all uppercase", () => {
+        expect(new Employee(eName, eId, eMail).getName()).toBe(eName.toUpperCase())
+    });
+    it("Gives employee ID matching input", () => {
+        expect(new Employee(eName, eId, eMail).getId()).toBe(eId)
+    });
+    it("Populates email address as passed to function", () =>{
+        expect(new Employee(eName, eId, eMail).getEmail()).toBe(eMail)
+    });
 
-module.exports = Employee;
+})
